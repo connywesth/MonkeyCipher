70a9a9e10b21f6cb91bff468356b88a342e77efb
Conny Westh <conny.westh@gmail.com>
2019-03-21 14:27:29 +0100

    Add files via upload

---
+++ KryptoQuiz/KryptoQuiz/MonkeyCipher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kodapor.KryptoQuiz
+{
+    public class MonkeyCipher
+    {
+        public static string EnCrypt(int key, string clearString)
+        {
+            int pos = 0;
+            int value = 0;
+            string cryptedText = "";
+            foreach (char ch in clearString)
+            {
+                value = ((int)ch) + key + pos;
+                cryptedText += $"{(char)(value)}";
+                pos++;
+            }
+            return cryptedText;
+        }
+
+        public static string DeCrypt(int key, string cryptedString)
+        {
+            int pos = 0;
+            int value = 0;
+            string deCryptedText = "";
+            foreach (char ch in cryptedString)
+            {
+                value = ((int)ch) - key - pos;
+                deCryptedText += $"{(char)value}";
+                pos++;
+            }
+            return deCryptedText;
+        }
+    }
+}
---
+++ KryptoQuiz/KryptoQuiz/Program.cs
@@ -0,0 +1,32 @@
+﻿// Kryperingsgåta av Conny Westh 2019-03-21, info@connywesth.se
+// Publicerad på Facebookgruppen Kodapor Off-Topic 2019-03-21.
+// Krypteringsalgoritmen är en modifierad varianrt av det över 2000 år gamla Ceasar-kryptot.
+//
+// Föreslagen lösning 2019-03-21 av Peter Eklund
+// https://github.com/dx-dt/monkeycipher/blob/master/cipher.ipynb?fbclid=IwAR1r_rL96eljw0fNbeMEtb5tBtlAMLuatUpvEkbLJjKYvd0xYFtd4pJuLAc
+
+using System;
+
+namespace Kodapor.KryptoQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kodapor Krypteringsgåta 2019-03-21!");
+            string clearText = "";
+            string cryptedString = "";
+            int key = 143;
+            const string originalText = "Katten Gustav är fetare än andra katter!";
+
+            Console.WriteLine($"Key: {key}, Original Text: {originalText}");
+
+            cryptedString = MonkeyCipher.EnCrypt(key: key, clearString: originalText);
+            Console.WriteLine($"Key: {key}, Crypted Text: {cryptedString}");
+
+            clearText = MonkeyCipher.DeCrypt(key: key, cryptedString: cryptedString);
+            Console.WriteLine($"Key: {key}, Decrypted Text: {clearText}");
+        }
+
+    }
+}
